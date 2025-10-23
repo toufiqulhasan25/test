fa452d9777baf29aad3e548ad4923bcee889d3e3
toufiqulhasan25 <toufiqulhasan25@gmail.com>
2025-10-23 12:24:40 +0600

    Add files via upload

---
+++ niyd-php/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'niyd-php/niyd-php',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '295d72ddc0a60ba05144d27440a76dd60087abbf',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'niyd-php/niyd-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '295d72ddc0a60ba05144d27440a76dd60087abbf',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
